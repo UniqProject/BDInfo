416f76dfa0b91772d80fe5e268705bb488891e54
UniqProject <JuergenTem@outlook.com>
2020-12-04 17:10:03 +0100

    Build for .NET-Framework 4.7.2; Enable High-DPI Awareness

--- BDInfo/FormMain.Designer.cs
+++ BDInfo/FormMain.Designer.cs
@@ -479,8 +479,8 @@ namespace BDInfo
             // FormMain
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(784, 575);
             this.Controls.Add(this.buttonIsoBrowse);
             this.Controls.Add(this.buttonRescan);
